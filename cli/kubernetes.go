7ccc4d3b5eb823e78296bf9c272499d7f04033aa
ArtemTrofimushkin <artemtrofimushkin@gmail.com>
2021-04-13 16:23:18 +0300

    Rework with go

---
+++ cli/kubernetes.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/kubectl/pkg/scheme"
+
+	// import auth plugins to make oidc auth work
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+)
+
+// Client is kubernetes client set wrapper
+type Client struct {
+	Namespace string
+	*kubernetes.Clientset
+	*rest.Config
+}
+
+// NewClient return new kubernetes client
+func NewClient(clientGetter genericclioptions.RESTClientGetter) (*Client, error) {
+	restConfig, err := clientGetter.ToRESTConfig()
+	if err != nil {
+		return nil, err
+	}
+	restConfig.GroupVersion = &schema.GroupVersion{Group: "", Version: "v1"}
+	if restConfig.APIPath == "" {
+		restConfig.APIPath = "/api"
+	}
+	if restConfig.NegotiatedSerializer == nil {
+		restConfig.NegotiatedSerializer = scheme.Codecs.WithoutConversion()
+	}
+	if len(restConfig.UserAgent) == 0 {
+		restConfig.UserAgent = rest.DefaultKubernetesUserAgent()
+	}
+
+	clientSet, err := kubernetes.NewForConfig(restConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	ns, _, err := clientGetter.ToRawKubeConfigLoader().Namespace()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Client{ns, clientSet, restConfig}, nil
+}
---
+++ cli/options.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+
+	flag "github.com/spf13/pflag"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/json"
+	kube "k8s.io/cli-runtime/pkg/genericclioptions"
+)
+
+type ToggleOptions struct {
+	Service string
+	Kube    *kube.ConfigFlags
+}
+
+type Patch struct {
+	Operation string      `json:"op"`
+	Path      string      `json:"path"`
+	Value     interface{} `json:"value,omitempty"`
+}
+
+func (opts *ToggleOptions) Parse() *flag.FlagSet {
+	fs := flag.NewFlagSet("options", flag.ExitOnError)
+	opts.Kube = &kube.ConfigFlags{
+		KubeConfig:  stringptr(""),
+		ClusterName: stringptr(""),
+		Context:     stringptr(""),
+		Namespace:   stringptr(""),
+	}
+	namespace, _, err := opts.Kube.ToRawKubeConfigLoader().Namespace()
+	if err == nil {
+		opts.Kube.Namespace = stringptr(namespace)
+	}
+	opts.Kube.AddFlags(fs)
+	return fs
+}
+
+func (opts *ToggleOptions) Patch(patch *Patch) error {
+	k8s, err := NewClient(opts.Kube)
+	if err != nil {
+		return err
+	}
+
+	body, err := encode(patch)
+	if err != nil {
+		return err
+	}
+
+	options := metav1.PatchOptions{
+		FieldManager: "kubectl-patch",
+	}
+	_, err = k8s.
+		CoreV1().
+		Services(*opts.Kube.Namespace).
+		Patch(context.Background(), opts.Service, types.JSONPatchType, body, options)
+	return err
+}
+
+func encode(patch *Patch) ([]byte, error) {
+	data := [1]Patch{*patch}
+	return json.Marshal(data)
+}
+
+func stringptr(s string) *string {
+	return &s
+}
